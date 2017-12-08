158ea85f8933fc8ed1153119c449c5134f590f74
joshwestbury <joshwestbury@gmail.com>
2017-12-08 09:51:02 -0600

    added header, footer, preview, and search components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { DisplayMovieComponent } from './display-movie.component';
+import { SearchComponent } from './search/search.component';
+import { MoviePreviewComponent } from './search/movie-preview/movie-preview.component';
+import { FooterComponent } from './footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DisplayMovieComponent,
+    SearchComponent,
+    MoviePreviewComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-header',
+    template:  `
+        <span>header</span>
+    `
+})
+
+export class HeaderComponent {
+
+}
